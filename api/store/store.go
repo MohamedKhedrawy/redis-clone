a2840856b9de433a689c2c88e47f22e7b965e820
term <medomoataz2hof@gmail.com>
2025-12-27 20:57:50 +0200

    Add the store for all kv pairs and methods for managing it

---
+++ api/store/store.go
@@ -0,0 +1,96 @@
+package store
+
+import (
+	"sync"
+	"time"
+	"strings"
+)
+
+type Store struct {
+	data map[string]Value
+	mu   sync.RWMutex
+}
+
+type Value struct {
+	dataValue string
+	expiresAt time.Time
+}
+
+func (v *Value) GetValue() string {
+	return v.dataValue
+}
+
+func NewStore() *Store {
+	return &Store{
+		data: make(map[string]Value),
+		mu:   sync.RWMutex{},
+	}
+}
+
+func (s *Store) Get(key string) (Value, bool, error) {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	value, exists := s.data[key]
+	if value.expiresAt.IsZero() == false && time.Now().After(value.expiresAt) {
+		// Key has expired
+		delete(s.data, key)
+		return Value{}, false, nil
+	}
+	return value, exists, nil
+}
+
+func (s *Store) Set(args []string) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	key := args[0]
+	value := args[1]
+	if len(args) == 4 && strings.ToUpper(args[2]) == "EX" {
+		expireSeconds, err := time.ParseDuration(args[3] + "s")
+		if err != nil {
+			return err
+		}
+		s.data[key] = Value{dataValue: value, expiresAt: time.Now().Add(expireSeconds)}
+		return nil
+	}
+	if len(args) == 4 && strings.ToUpper(args[2]) == "PX" {
+		expireMilliseconds, err := time.ParseDuration(args[3] + "ms")
+		if err != nil {
+			return err
+		}
+		s.data[key] = Value{dataValue: value, expiresAt: time.Now().Add(expireMilliseconds)}
+		return nil
+	}
+	s.data[key] = Value{dataValue: value, expiresAt: time.Time{}}
+	return nil
+}
+
+func (s *Store) Delete(key string) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	delete(s.data, key)
+	return nil
+}
+
+func (s *Store) Keys() []string {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	keys := make([]string, 0, len(s.data))
+	for key := range s.data {
+		keys = append(keys, key)
+	}
+	return keys
+}
+
+func (s *Store) Clear() error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.data = make(map[string]Value)
+	return nil
+}
+
+func (s *Store) Exists(key string) (bool, error) {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	_, exists := s.data[key]
+	return exists, nil
+}
